3aa9fe0b8a50a5512469d5e3929b4e403baa5746
JessJudd <jessica@brand-knew.com>
2020-11-16 10:00:43 -0800

    New About Page

---
+++ wp-content/themes/ufc/acf/fields/about.php
@@ -0,0 +1,398 @@
+<?php
+
+if( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array(
+        'key' => 'about2020',
+        'title' => 'About Page',
+        'style' => 'seamless',
+        'position' => 'acf_after_title',
+        'fields' => array (
+            /* Hero */
+            array(
+                'key' => 'about2020__hero__tab',
+                'label' => 'Hero',
+                'type' => 'tab',
+            ),
+            array(
+                'key' => 'about2020__hero__d_img',
+                'label' => 'Desktop Background Image',
+                'name' => 'hero_image_d',
+                'type' => 'image',
+                'preview_size' => 'thumbnail',
+                'return_format' => 'url',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__hero__m_img',
+                'label' => 'Mobile Background Image',
+                'name' => 'hero_image_m',
+                'type' => 'image',
+                'preview_size' => 'thumbnail',
+                'return_format' => 'url',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__hero__headline',
+                'label' => 'Headline',
+                'name' => 'hero_headline',
+                'type' => 'text',
+                'default_value' => 'About United Friends',
+                'required' => 1,
+            ),
+
+            /* Subhero */
+            array(
+                'key' => 'about2020__subhero__tab',
+                'label' => 'Subhero',
+                'type' => 'tab',
+            ),
+            array(
+                'key' => 'about2020__subhero__copy',
+                'label' => 'Copy',
+                'name' => 'subhero_copy',
+                'type' => 'textarea',
+                'rows' => 2,
+                'required' => 1
+            ),
+
+            /* Section 1 */
+            array(
+                'key' => 'about2020__s1__tab',
+                'label' => 'Section 1',
+                'type' => 'tab',
+            ),
+            array(
+                'key' => 'about2020__s1__headline',
+                'label' => 'Headline',
+                'name' => 's1_headline',
+                'type' => 'text',
+                'default_value' => 'Our Guiding Principles',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__s1__featured',
+                'label' => 'Section 1 Featured Content',
+                'name' => 's1_featured',
+                'type' => 'repeater',
+                'layout' => 'row',
+                'min' => 3,
+                'max' => 3,
+                'button_label' => 'Add Card',
+                'sub_fields' => array(
+                    array(
+                        'key' => 'featured__header',
+                        'label' => 'Header',
+                        'name' => 's1_header',
+                        'type' => 'text',
+                        'required' => 1,
+                    ),
+                    array(
+                        'key' => 'featured__copy',
+                        'label' => 'Copy',
+                        'name' => 's1_copy',
+                        'type' => 'textarea',
+                        'rows' => 2,
+                        'required' => 1
+                    ),
+                ),
+            ),
+
+            /* Section 2 */
+            array(
+                'key' => 'about2020__s2__tab',
+                'label' => 'Section 2',
+                'type' => 'tab',
+            ),
+            array(
+                'key' => 'about2020__s2__headline',
+                'label' => 'Headline',
+                'name' => 's2_headline',
+                'type' => 'text',
+                'default_value' => 'History',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__s2__copy',
+                'label' => 'Copy',
+                'name' => 's2_copy',
+                'type' => 'textarea',
+                'rows' => 2,
+                'required' => 1
+            ),
+            array(
+                'key' => 'about2020__s2__button_url',
+                'label' => 'Button URL',
+                'name' => 's2_button_url',
+                'type' => 'url',
+                'instructions' => 'If empty, this button will not show.'
+            ),
+            array(
+                'key' => 'about2020__s2__button_text',
+                'label' => 'Button Text',
+                'name' => 's2_button_text',
+                'type' => 'text',
+                'instructions' => 'If empty, this button will not show.'
+            ),
+            array(
+                'key' => 'about2020__s2button_target',
+                'label' => 'Button Target',
+                'name' => 's2_button_target',
+                'type' => 'true_false',
+                'message' => 'Open link in a new tab'
+            ),
+            array(
+                'key' => 'about2020__s2__d_img',
+                'label' => 'Desktop Background Image',
+                'name' => 's2_image_d',
+                'type' => 'image',
+                'preview_size' => 'thumbnail',
+                'return_format' => 'url',
+                'required' => 1,
+            ),
+
+            /* Section 3 */
+            array(
+                'key' => 'about2020__s3__tab',
+                'label' => 'Section 3',
+                'type' => 'tab',
+            ),
+            array(
+                'key' => 'about2020__s3__headline',
+                'label' => 'Headline',
+                'name' => 's3_headline',
+                'type' => 'text',
+                'default_value' => 'United Friends Serves 1,400 Current and Former Foster Youth Annually',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__s3__left__title',
+                'label' => 'Left Chart Title',
+                'name' => 's3_left_title',
+                'type' => 'text',
+                'required' => 1,
+                'wrapper' => array (
+                    'width' => '25',
+                ),
+            ),
+            array(
+                'key' => 'about2020__s3__left__img',
+                'label' => 'Left Chart Image',
+                'name' => 's3_left_img',
+                'type' => 'image',
+                'preview_size' => 'thumbnail',
+                'return_format' => 'url',
+                'required' => 1,
+                'wrapper' => array (
+                    'width' => '25',
+                ),
+            ),
+            array(
+                'key' => 'about2020__s3__right__title',
+                'label' => 'Right Chart Title',
+                'name' => 's3_right_title',
+                'type' => 'text',
+                'required' => 1,
+                'wrapper' => array (
+                    'width' => '25',
+                ),
+            ),
+            array(
+                'key' => 'about2020__s3__right__img',
+                'label' => 'Right Chart Image',
+                'name' => 's3_right_img',
+                'type' => 'image',
+                'preview_size' => 'thumbnail',
+                'return_format' => 'url',
+                'required' => 1,
+                'wrapper' => array (
+                    'width' => '25',
+                ),
+            ),
+            array(
+                'key' => 'about2020__s3_demographics_repeat',
+                'label' => 'Demographics',
+                'name' => 's3_demographics_repeat',
+                'type' => 'repeater',
+                'layout' => 'row',
+                'min' => 1,
+                'button_label' => 'Add Demographic',
+                'sub_fields' => array(
+                    array(
+                        'key' => 'about2020__s3_demographics_color',
+                        'label' => 'Demographic Label Color',
+                        'name' => 's3_color',
+                        'type' => 'select',
+                        'required' => 1,
+                        'default' => '49',
+                        'choices' => array(
+                            'blue' => 'Blue',
+                            'gold' => 'Gold',
+                            'light-blue' => 'Light Blue',
+                            'maroon' => 'Maroon',
+                            'orange' => 'Orange',
+                            'pink' => 'Pink',
+                        ),
+                        'wrapper' => array (
+                            'width' => '25',
+                        ),
+                    ),
+                    array(
+                        'key' => 'about2020__s3_demographics_label',
+                        'label' => 'Demographic Label Name',
+                        'name' => 's3_label',
+                        'type' => 'text',
+                        'required' => 1,
+                        'default' => '49',
+                        'wrapper' => array (
+                            'width' => '25',
+                        ),
+                    ),
+                    array(
+                        'key' => 'about2020__s3_demographics_value',
+                        'label' => 'Demographic Percentage',
+                        'name' => 's3_value',
+                        'type' => 'text',
+                        'required' => 1,
+                        'default' => '49',
+                        'instructions' => 'The % symbol is included, please only the type the number value.',
+                        'wrapper' => array (
+                            'width' => '25',
+                        ),
+                    ),
+                ),
+            ),
+            /* Section 4 */
+            array(
+                'key' => 'about2020__s4__tab',
+                'label' => 'Section 4',
+                'type' => 'tab',
+            ),
+            array(
+                'key' => 'about2020__s4__d_img',
+                'label' => 'Desktop Background Image',
+                'name' => 's4_image_d',
+                'type' => 'image',
+                'preview_size' => 'thumbnail',
+                'return_format' => 'url',
+                'required' => 1,
+            ),
+            // array(
+            //     'key' => 'about2020__s4__m_img',
+            //     'label' => 'Mobile Background Image',
+            //     'name' => 's4_image_m',
+            //     'type' => 'image',
+            //     'preview_size' => 'thumbnail',
+            //     'return_format' => 'url',
+            //     'required' => 1,
+            // ),
+
+            /* Section 5 */
+            array(
+                'key' => 'about2020__s5__tab',
+                'label' => 'Section 5',
+                'type' => 'tab',
+            ),
+            array(
+                'key' => 'about2020__s5__left__headline',
+                'label' => 'Left Headline',
+                'name' => 's5_left_headline',
+                'type' => 'text',
+                'default_value' => 'Financials',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__s5__left__copy',
+                'label' => 'Left Copy',
+                'name' => 's5_left_copy',
+                'type' => 'textarea',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__s5__left__button_url',
+                'label' => 'Left Button URL',
+                'name' => 's5_left_button_url',
+                'type' => 'url',
+                'instructions' => 'If empty, this button will not show.'
+            ),
+            array(
+                'key' => 'about2020__s5__left__button_text',
+                'label' => 'Left Button Text',
+                'name' => 's5_left_button_text',
+                'type' => 'text',
+                'default_value' => 'Explore',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__s5__left__button_target',
+                'label' => 'Left Button Target',
+                'name' => 's5_left_button_target',
+                'type' => 'true_false',
+                'message' => 'Open link in a new tab'
+            ),
+            array(
+                'key' => 'about2020__s5__right__headline',
+                'label' => 'Right Headline',
+                'name' => 's5_right_headline',
+                'type' => 'text',
+                'default_value' => 'Leadership',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__s5__right__copy',
+                'label' => 'Right Copy',
+                'name' => 's5_right_copy',
+                'type' => 'textarea',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__s5__right__button_url',
+                'label' => 'Right Button URL',
+                'name' => 's5_right_button_url',
+                'type' => 'url',
+                'instructions' => 'If empty, this button will not show.'
+            ),
+            array(
+                'key' => 'about2020__s5__right__button_text',
+                'label' => 'Right Button Text',
+                'name' => 's5_right_button_text',
+                'type' => 'text',
+                'default_value' => 'View All',
+                'required' => 1,
+            ),
+            array(
+                'key' => 'about2020__s5__right__button_target',
+                'label' => 'Right Button Target',
+                'name' => 's5_right_button_target',
+                'type' => 'true_false',
+                'message' => 'Open link in a new tab'
+            ),
+        ),
+        'location' => array (
+            array (
+                array (
+                    'param' => 'page_template',
+                    'operator' => '==',
+                    'value' => 'p-about-2020.php',
+                ),
+            ),
+        ),
+        'hide_on_screen' => array(
+            0 => 'the_content',
+            1 => 'excerpt',
+            2 => 'custom_fields',
+            3 => 'discussion',
+            4 => 'comments',
+            5 => 'slug',
+            6 => 'author',
+            7 => 'format',
+            8 => 'featured_image',
+            9 => 'categories',
+            10 => 'tags',
+            11 => 'send-trackbacks',
+        ),
+    ));
+
+endif;
+
+?>
---
+++ wp-content/themes/ufc/p-about-2020.php
@@ -0,0 +1,260 @@
+<?
+    /*
+	    Template Name: About 2020
+	*/
+
+	get_header();
+
+	the_post();
+
+    global $post;
+
+    /* acf fields */
+    $hero__img__d = get_field('hero_image_d');
+    $hero__img__m = get_field('hero_image_m');
+    $hero__headline = get_field('hero_headline');
+
+    $subhero__blurb = get_field('subhero_copy');
+
+    $s1__headline = get_field('s1_headline');
+
+    $s2__headline = get_field('s2_headline');
+    $s2__img = get_field('s2_image_d');
+    $s2__copy = get_field('s2_copy');
+    $s2__button_url = get_field('s2_button_url');
+    $s2__button_text = get_field('s2_button_text');
+    $get_s2__button_target = get_field('s2_button_target');
+    $s2__button_target = ($get_s2__button_target == 1) ? '_blank' : '';
+
+    $s3__headline = get_field('s3_headline');
+    $s3__title_left = get_field('s3_left_title');
+    $s3__img_left = get_field('s3_left_img');
+    $s3__title_right = get_field('s3_right_title');
+    $s3__img_right = get_field('s3_right_img');
+    $s3__demographics = array(
+        get_field('about2020__s3_demographics_hispanic'),
+        get_field('about2020__s3_demographics_black'),
+        get_field('about2020__s3_demographics_multi'),
+        get_field('about2020__s3_demographics_white'),
+        get_field('about2020__s3_demographics_na'),
+        get_field('about2020__s3_demographics_asian'),
+        get_field('about2020__s3_demographics_native')
+    );
+
+    $s4__bg_img = get_field('s4_image_d');
+
+    $s5__left__bg_color = get_field('s5_left_bg');
+    $s5__left__headline = get_field('s5_left_headline');
+    $s5__left__copy = get_field('s5_left_copy');
+    $s5__left__button_url = get_field('s5_left_button_url');
+    $s5__left__button_text = get_field('s5_left_button_text');
+    $get_s5__left__button_target = get_field('s5_left_button_target');
+    $s5__left__button_target = ($get_s5__left__button_target == 1) ? '_blank' : '';
+
+    $s5__right__bg_color = get_field('s5_right_bg');
+    $s5__right__headline = get_field('s5_right_headline');
+    $s5__right__copy = get_field('s5_right_copy');
+    $s5__right__button_url = get_field('s5_right_button_url');
+    $s5__right__button_text = get_field('s5_right_button_text');
+    $get_s5__right__button_target = get_field('s5_right_button_target');
+    $s5__right__button_target = ($get_s5__right__button_target == 1) ? '_blank' : '';
+
+
+    $css = '<style type="text/css">
+    @media screen and (min-width:769px){
+        #home__hero {
+            background-image:url('. $hero__img__d .')!important;
+        }
+    }
+    </style>';
+
+    echo $css;
+
+?>
+
+<div id="main" class="grid">
+    <div id="home__hero" class="about2020 row bg-shaded bg-cover" style="background-image:url(<?php echo $hero__img__m; ?>);background-color:#70bae3;">
+        <div id="home__hero--inner" class="c12 white">
+            <div id="home__hero--content" class="text-center" style="background-color:#70bae3;">
+                <h2 class="size10 margin-bot20"><?php echo $hero__headline; ?></h2>
+            </div>
+        </div>
+    </div>
+    <?php if ($subhero__blurb){
+
+        $subhero = '
+        <div id="home__subhero" class="row about2020">
+            <div id="home__subhero--inner" class="c12">
+                <div id="home__subhero--content" class="white text-center">
+                    <p class="size5 margin-bot20">' . $subhero__blurb .'</p>
+                </div>
+            </div>
+        </div>
+        ';
+        echo $subhero;
+    } ?>
+
+    <?php
+    if( $s1__headline ) : ?>
+        <div class="about__featured">
+            <div class="about__featured-title">
+                <h2 class="about__featured-header">
+                    <?php echo $s1__headline; ?>
+                </h2>
+            </div>
+            <!-- start row-->
+            <div id="featured-wrapper" class="row">
+
+            <?php if(have_rows('s1_featured')):
+                while(have_rows('s1_featured')) : the_row();
+
+                // Load sub field value.
+                $title = get_sub_field('s1_header');
+                $copy = get_sub_field('s1_copy');
+                $card = '<div class="c4 card">
+                    <h3>'.$title.'</h3>
+                    <p>'.$copy.'</p>
+                </div>';
+                echo $card;
+
+                endwhile;
+            endif; ?>
+
+            </div>
+            <!-- end row -->
+        </div>
+    <?php
+    endif;
+    ?>
+
+    <?php
+        if( $s2__headline ) :
+    ?>
+        <div class="about__history row" style="background-color:#70bae3;">
+            <div class="c6 about__history--left">
+                <div class="about__history--inner white">
+                    <h3 class="size10 bold margin-bot20"><?php echo $s2__headline; ?></h3>
+                    <p class="size5 about__history--copy"><?php echo $s2__copy; ?></p>
+                    <?php if($s2__button_url != ''): ?>
+                    <a href="<?php echo $s2__button_url; ?>" class="btn btn-blue" <?php echo $s2__button_target; ?>><?php echo $s2__button_text; ?></a>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="c6 about__history--right" style="background-image:url(<?php echo $s2__img; ?>);"></div>
+        </div>
+    <?php endif; ?>
+
+    <?php 
+        if( $s3__headline ) :
+    ?>
+
+    <div class="about__charts">
+        <div class="about__charts-title">
+            <h2 class="about__charts-header">
+                <?php echo $s3__headline; ?>
+            </h2>
+        </div>
+        <div class="row">
+            <div class="about__chart-col about__chart-col--left">
+                <?php 
+                    echo '<h4 class="about__charts-label">'.$s3__title_left.'</h4>
+                    <img class="nostretch about__charts-img about__charts-img--left" src="'.$s3__img_left.'" alt="'.$s3__title_left.'" />';
+                ?>
+            </div>
+            <div class="about__chart-col about__chart-col--gutter"></div>
+            <div class="about__chart-col about__chart-col--right demographics">
+                <?php 
+                    echo '<h4 class="about__charts-label">'.$s3__title_right.'</h4>';
+                ?>
+                <div class="demo__inner">
+                    <?php 
+                        echo '
+                        <img class="nostretch about__charts-img about__charts-img--right" src="'.$s3__img_right.'" alt="'.$s3__title_right.'" />';
+
+                        if( have_rows('s3_demographics_repeat') ):
+                            // Loop through rows.
+                            echo '<ul class="demo__list">';
+                            while( have_rows('s3_demographics_repeat') ) : the_row();
+                                // Load sub field value.
+                                $color = get_sub_field('s3_color');
+                                $label = get_sub_field('s3_label');
+                                $value = get_sub_field('s3_value');
+
+                                echo '<li class="demo__list-item">
+                                <span class="demo__color '.$color.'"></span>
+                                <span class="demo__name">'.$label.'</span>
+                                <span class="demo__stat">'.$value.'%</span>
+                                </li>';
+
+                                // Do something...
+                            // End loop.
+                            endwhile;
+                            echo '</ul>';
+                            // No value.
+                            else :
+                            // Do something...
+                            endif;
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php endif; 
+    
+    if ($s4__bg_img) :
+
+        echo '<div class="about__divider">
+            <div class="about__divider--img" style="background-image:url('.$s4__bg_img.');">
+            </div>
+        </div>';
+
+    endif;
+    
+    
+    ?>
+
+    <div class="home__overlay row">
+        <div class="c6 home__overlay--left" style="background-color:#5c0f40;">
+            <div class="home__overlay--inner white">
+                <h3 class="size10 bold margin-bot20"><?php echo $s5__left__headline; ?></h3>
+                <p class="size5 home__overlay--copy"><?php echo $s5__left__copy; ?></p>
+                <?php if($s5__left__button_url != ''): ?>
+                <a href="<?php echo $s5__left__button_url; ?>" class="btn btn-blue" <?php echo $s5__left__button_target; ?>><?php echo $s5__left__button_text; ?></a>
+                <?php endif; ?>
+            </div>
+        </div>
+        <div class="c6 home__overlay--right" style="background-color:#70bae3;">
+            <div class="home__overlay--inner white">
+                <h3 class="size10 bold margin-bot20"><?php echo $s5__right__headline; ?></h3>
+                <p class="size5 home__overlay--copy"><?php echo $s5__right__copy; ?></p>
+                <?php if($s5__right__button_url != ''): ?>
+                <a href="<?php echo $s5__right__button_url; ?>" class="btn btn-blue" <?php echo $s5__right__button_target; ?>><?php echo $s5__right__button_text; ?></a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+
+    <?php if($instagram == 1): ?>
+    <div class="home__instagram row">
+        <div class="c12">
+            <a id="instaIcon" href="https://www.instagram.com/unitedfriendsla/" target="_blank">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/instagram-2x.png" alt="Instagram" />
+            </a>
+            <h3>Connect with United Friends</h3>
+            <div id="igWrapper">
+                <div id="igBackground">
+                    <div id="loader">
+                        <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                    </div>
+                </div>
+                <div id="instaFade">
+                    <div id="instafeed"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
+</div>
+
+<?	get_footer(); 	?>
